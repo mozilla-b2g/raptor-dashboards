52f118c23da0071fb845272c6ff24743e221379d
Eli Perelman <eli@eliperelman.com>
2015-10-12 11:07:09 -0500

    Attempting to templating SELECT aggregations

---
+++ public/dashboards/src/aggregate.js
@@ -0,0 +1,37 @@
+define(function() {
+
+  return function(measurement) {
+
+    return {
+      "allFormat": "glob",
+      "current": {},
+      "datasource": null,
+      "includeAll": false,
+      "label": "Aggregate",
+      "multi": false,
+      "multiFormat": "regex values",
+      "name": "aggregate",
+      "options": [
+        {
+          "text": "95% Bound",
+          "value": "MEAN(value) + (1.96 * STDDEV(value) / 5.477) as value",
+          "selected": true
+        },
+        {
+          "text": "Mean",
+          "value": "MEAN(value) as value",
+          "selected": false
+        },
+        {
+          "text": "Values",
+          "value": "value",
+          "selected": false
+        }
+      ],
+      "query": null,
+      "refresh": true,
+      "refresh_on_load": true,
+      "type": "custom"
+    };
+  };
+});
